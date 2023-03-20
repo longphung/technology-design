546c2cd86d439c2c5314efb2854c49a0950ad92f
Phung Nguyen <longphung.nn@gmail.com>
2023-03-20 13:00:28 +1100

    🔨 refactor: resive gif, remove unused import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const puppeteer = require("puppeteer");
-const {text} = require("express");
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
