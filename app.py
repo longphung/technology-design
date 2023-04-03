247d927d8f2c9e437739f3d987141767e5b5b64e
Phung Nguyen <longphung.nn@gmail.com>
2023-04-04 00:58:08 +1000

    ✨ feat: add /faqs/download endpoint

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
+from io import StringIO
+from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,16 +10,10 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@app.get("/")
-def hello_world():
-    return render_template('index.html')
-
-
-@app.get('/faqs')
-def faqs():
+def get_faqs():
     response = requests.get('https://www.swinburneonline.edu.au/faqs/')
     soup = BeautifulSoup(response.content, 'html.parser')
-    # Get faq cards
+    # get faq cards
     faqs_cards = soup.select('.faqs-group .card')
     result = []
     # loop through and get questions and answers
@@ -30,6 +26,42 @@ def faqs():
             answer = answer_el.get_text(strip=True)
             result.append((question, answer))
     return result
+
+
+@app.get("/")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.get('/faqs')
+def faqs():
+    return get_faqs()
+
+
+@app.get('/faqs/download')
+def download_faqs():
+    questions_answers = get_faqs()
+
+    def generate_csv():
+        in_memory_file = StringIO()
+        csv_writer = csv.writer(in_memory_file)
+        csv_writer.writerow(['Questions', 'Answers'])
+        for question, answer in questions_answers:
+            # write the row to the in-memory object
+            csv_writer.writerow([question, answer])
+            csv_row = in_memory_file.getvalue()
+            # reset the in-memory object for the next row
+            in_memory_file.seek(0)
+            in_memory_file.truncate(0)
+            yield csv_row
+
+    return Response(
+        generate_csv(),
+        content_type='text/csv',
+        headers={
+            'Content-Disposition': 'attachment; filename=faqs.csv'
+        }
+    )
 
 
 @socketio.on('message')
