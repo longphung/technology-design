6575002225489801b0c4023cca624a7638cef224
Phung Nguyen <longphung.nn@gmail.com>
2023-04-03 11:20:42 +1000

    ✨ feat: parse swinburne faq with faqs endpoint

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -6,9 +8,28 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@app.route("/")
+@app.get("/")
 def hello_world():
     return render_template('index.html')
+
+
+@app.get('/faqs')
+def faqs():
+    response = requests.get('https://www.swinburneonline.edu.au/faqs/')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Get faq cards
+    faqs_cards = soup.select('.faqs-group .card')
+    result = []
+    # loop through and get questions and answers
+    for faq in faqs_cards:
+        question_el = faq.select_one('.card-header h5 > div:nth-child(2)')
+        answer_el = faq.select_one('.card-body .content')
+        # add to result if question and answer exist
+        if question_el and answer_el:
+            question = question_el.get_text(strip=True)
+            answer = answer_el.get_text(strip=True)
+            result.append((question, answer))
+    return result
 
 
 @socketio.on('message')
