1bc38287041cc744369127df8ed65eb5c5474c9c
Phung Nguyen <longphung.nn@gmail.com>
2023-05-01 00:35:44 +1000

    ✨ feat: extract get_faqs function

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
 import csv
-import requests
-from bs4 import BeautifulSoup
 from io import StringIO
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, emit
 
+from utils import swinburne_utils
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-
-
-def get_faqs():
-    response = requests.get('https://www.swinburneonline.edu.au/faqs/')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # get faq cards
-    faqs_cards = soup.select('.faqs-group .card')
-    result = []
-    # loop through and get questions and answers
-    for faq in faqs_cards:
-        question_el = faq.select_one('.card-header h5 > div:nth-child(2)')
-        answer_el = faq.select_one('.card-body .content')
-        # add to result if question and answer exist
-        if question_el and answer_el:
-            question = question_el.get_text(strip=True)
-            answer = answer_el.get_text(strip=True)
-            result.append((question, answer))
-    return result
 
 
 @app.get("/")
@@ -35,12 +17,12 @@ def hello_world():
 
 @app.get('/faqs')
 def faqs():
-    return get_faqs()
+    return swinburne_utils.get_faqs()
 
 
 @app.get('/faqs/download')
 def download_faqs():
-    questions_answers = get_faqs()
+    questions_answers = swinburne_utils.get_faqs()
 
     def generate_csv():
         in_memory_file = StringIO()
